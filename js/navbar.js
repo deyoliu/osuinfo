92219f0303ce85760a68c188aa0f0dfb0aed94ef
deyomoe <icytape620@gmail.com>
2020-09-21 17:00:36 +0800

    +userpage

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,28 @@
-; (function () {
-    $('.dropdown-menu').mouseleave(function () {
-        $(this).removeClass('show');
-        $('.dropdown').removeClass('show');
-    })
+(function () {
+    //nav fixed top
+    let nav = document.querySelector(".navbar");
+    let navPosition = nav.offsetTop;
+    console.log(navPosition);
+    function scrollHandler() {
+        if (window.scrollY >= navPosition) {
+            //  fixed menu
+            document.body.classList.add("fixedMenu");
+            document.body.style.paddingTop = nav.offsetHeight + "px";
+        } else {
+            //  normal
+            document.body.classList.remove("fixedMenu");
+            document.body.style.paddingTop = "";
+        }
+    }
+    window.addEventListener("scroll", scrollHandler);
+    window.addEventListener("resize", () => {
+        navPosition = nav.offsetTop;
+    });
+})();
 
 
-}());
+//navbar hover
+$('.dropdown-menu').mouseleave(function () {
+    $(this).removeClass('show');
+    $('.dropdown').removeClass('show');
+})
