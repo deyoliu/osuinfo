fa1ed884ce6bbc72ebd84745443bc85fe436c472
deyomoe <icytape620@gmail.com>
2020-09-30 23:10:48 +0800

    add flags

--- js/user.js
+++ js/user.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var uid = 147515;
+    var uid = 654296;
     //personal stats
     $.ajax({
         url: "https://osu.ppy.sh/api/get_user?k=c4e57a353c571a5a2e4e2d0861c667a0e626a0a0&u=" + uid,
@@ -11,7 +11,8 @@ $(document).ready(function () {
             $('#ps-rank').text("#" + data[0].pp_rank);
             $('#ps-pp').text(data[0].pp_raw);
             $('#ps-pc').text(data[0].playcount);
-            $('#avatar').attr("src", "http://s.ppy.sh/a/" + uid)
+            $('#avatar').attr("src", "http://s.ppy.sh/a/" + uid);
+            $('#country').attr("src", "../imgs/country-flags/" + data[0].country + ".png").attr("alt", data[0].country);
         },
         error: function (xhr, status, error) {
             console.log(error);
